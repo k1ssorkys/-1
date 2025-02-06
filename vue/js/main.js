13951026cb406659ea26e8f5bd9ee831080c8f83
k1ssorkys <crackcobain34@gmail.com>
2025-02-06 11:45:54 +0700

    1

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,13 +1,28 @@
-
 let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: true
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        inventory: 100,
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
+
+
 })
+
 
 
 
